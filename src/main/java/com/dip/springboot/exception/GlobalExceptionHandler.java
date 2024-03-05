df5e30266acedfb553bcbcbf9ff4bf9fc9a0d1e8
Diptendu <diptendu.tu@gmail.com>
2024-03-05 18:58:31 +0530

    Handle All Other  Exception

--- src/main/java/com/dip/springboot/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dip/springboot/exception/GlobalExceptionHandler.java
@@ -35,4 +35,17 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "INTERNAL SERVER EXCEPTION"
+
+        );
+
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
