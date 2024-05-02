91e23a6942b3a185a1f63944b02ee2d02cd78f7a
Diptendu <diptendu.tu@gmail.com>
2024-05-03 00:23:09 +0530

    Spring SecurityConfiguration

--- src/main/java/com/dip/springboot/controller/HelloWorldController.java
+++ src/main/java/com/dip/springboot/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.dip.springboot.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     //HTTP GET
-    //htttp:localhost:8080/hello
+    //http:localhost:8080/hello
     @GetMapping("/hello")
     public  String helloWorld(){
-        return "hello World !!";
+
+        var res = "<h1>Hello World</h1>";
+        return res;
     }
 }
